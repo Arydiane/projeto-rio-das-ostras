9b94720eaab79706a22ed3bbf2d3ee911104c102
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-10-07 17:16:48 -0300

    adiciona função dark mode

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const btnMenu = document.querySelector('.header__menu-hamburger')
+const btnTheme = document.querySelector('.header__button-theme')
 const menu = document.querySelector('.header__navbar')
 
 btnMenu.addEventListener('click', () => {
     menu.classList.toggle('header__navbar--ativo')
 })
+
+btnTheme.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode')
+})
+
+
