1d8dc64007f6e53305b06d95a8e3b031fa801b7c
Arydiane <103674349+Arydiane@users.noreply.github.com>
2022-10-10 11:17:48 -0300

    corrige a função dark mode
    Adiciona a troca de imagem do botão entre sol e lua.

--- js/toggle-theme.js
+++ js/toggle-theme.js
@@ -2,4 +2,11 @@
 const btnTheme = document.querySelector('.header__button-theme')
 btnTheme.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode')
+    const imgBtnTheme =  btnTheme.firstElementChild.innerHTML
+    //alterna a imagem do botão de tema
+    if ( imgBtnTheme == "dark_mode") {
+        btnTheme.firstElementChild.innerHTML = "light_mode"
+    } else {
+        btnTheme.firstElementChild.innerHTML = "dark_mode"
+    }
 })
